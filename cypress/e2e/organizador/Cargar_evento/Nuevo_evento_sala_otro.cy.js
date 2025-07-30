f2c6687e2a7f5859b945a4d7bb47c5800e02d361
Fiogonzalezc <166340998+Fiogonzalezc@users.noreply.github.com>
2025-07-29 22:43:54 -0300

    Nuevo evento y preventa

---
+++ cypress/e2e/organizador/Cargar_evento/Nuevo_evento_sala_otro.cy.js
@@ -0,0 +1,65 @@
+describe('Creacion de evento de inicio a fin con 1 fecha y 1 funcion', () => {
+  it('debería loguearse con credenciales válidas', () => {
+    const tituloAleatorio = `evento${Math.floor(Math.random() * 10000)}`;
+    const hora = String(Math.floor(Math.random() * 24)).padStart(2, '0'); // entre 00 y 23
+    const minutos = String(Math.floor(Math.random() * 60)).padStart(2, '0'); // entre 00 y 59
+    const duracionEvento = `${hora}:${minutos}`;
+    
+    cy.visit('/');
+    cy.contains('Login').click({ force: true });
+    cy.get('input[type="email"]').type('himexxiii@gmail.com');
+    cy.get('input[type="password"]').type('Organizador9!');
+    cy.contains('Login').click();
+    cy.get('button[aria-label="Toggle menu"]').click();
+    cy.get(':nth-child(2) > .pb-4').click();
+    cy.get('[data-cy="input-titulo"]').type(tituloAleatorio);
+
+     const randomMes = Math.floor(Math.random() * 12) + 1;
+    const diaMes = new Date(2025, randomMes, 0).getDate();
+    const randomDia = Math.floor(Math.random() * diaMes) + 1;
+    const año = '2025';
+
+    cy.get('[data-type="day"]').first().focus().type('{selectall}{backspace}').type(randomDia, { force: true });
+    cy.get('[data-type="month"]').first().focus().type('{selectall}{backspace}').type(randomMes, { force: true });
+    cy.get('[data-type="year"]').first().focus().type(año);
+
+    cy.get('[data-cy="select-edad"]').click();
+    cy.get('[data-cy="option-edad-ATP"]').click();
+    cy.get('[data-cy="select-genero"] > .inline-flex').click();
+    cy.get('[data-cy="option-genero-Fiesta"]').click();
+    
+    cy.get('[data-cy="input-horario"]').within(() => {
+    cy.get('[data-type="hour"]').first().focus().type('{selectall}{backspace}').type(hora, { force: true });
+    cy.get('[data-type="minute"]').first().focus().type('{selectall}{backspace}').type(minutos, { force: true })
+    })
+
+    cy.get('[data-cy="input-duracion"]').within(() => {
+    cy.get('[data-type="hour"]').first().focus().type('{selectall}{backspace}').type(hora, { force: true });
+    cy.get('[data-type="minute"]').first().focus().type('{selectall}{backspace}').type(minutos, { force: true })  
+    })
+
+     cy.get('[data-cy="select-lugar-evento"]').click();
+    cy.get('[data-cy="Otro"]').click()
+    cy.contains('Nombre del Lugar').type('Espacio Mascara')
+    cy.contains('Calle').type('Santa fe')
+    cy.contains('Altura').type('920')
+    cy.contains('Codigo Postal').type('xs5000')
+    cy.contains('Provincia').click()
+    cy.contains('Córdoba').click();
+    
+    const texto = 'a '.repeat(4);
+    cy.get('[data-cy="input-info"]').type(texto);
+    cy.wait(1000)
+
+    cy.get('.rounded-b-large > .z-0').click()
+
+  
+    cy.contains('Siguiente').click()
+    cy.wait(2000)
+    cy.contains('Siguiente').click()
+    cy.contains('Confirmar').click()
+    
+
+  });
+});
+
